49a026d2f3f336fb940c6bf7699e17390022d575
Godeta <Godeta61@gmail.com>
2021-05-24 21:35:20 +0200

    test d'un effet drôle avec three.js pour le about me

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,102 @@
-import React, {useEffect, useState} from 'react';
+import React, { Component} from 'react';
+/* { useEffect, useState }
 import sanityClient from '../client.js';
 import backgroundComputerScience from '../background_computer_science.jpg';
 import imageUrlBuilder from '@sanity/image-url';
-import BlockContent from '@sanity/block-content-to-react';
+import BlockContent from '@sanity/block-content-to-react';*/
 
+// toute la partie pour utiliser three.js
+import ReactDOM from "react-dom";
+import * as THREE from "three";
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+import backgroundImage from '../portfolio2021.png'
+
+/**
+ * ce que fait mon three.js
+ */
+class App extends Component {
+    
+    // animations avec three.js
+  componentDidMount() {
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    
+    // document.body.appendChild( renderer.domElement );
+    // use ref as a mount point of the Three.js scene instead of the document.body
+    this.mount.appendChild( renderer.domElement );
+    camera.position.z = 30;
+    
+    // objets à ajouter
+    var geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
+    var material = new THREE.MeshStandardMaterial( { color: 0xff6347  } );
+    var torus = new THREE.Mesh( geometry, material );
+    scene.add( torus );
+    
+    // pour ajouter beaucoup d'étoiles
+    function addStar() {
+    const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+    const material = new THREE.MeshStandardMaterial( {color: 0xffffff})
+    const star = new THREE.Mesh( geometry, material);
+    const [x,y,z] = Array(3).fill().map( ()=> THREE.MathUtils.randFloatSpread(100));
+    star.position.set(x,y,z);
+    scene.add(star);
+    }
+    Array(200).fill().forEach(addStar);
+
+    // ajouter des textures (image de fond au lieu du vide par exemple)
+    const spaceTexture = new THREE.TextureLoader().load(backgroundImage);
+    scene.background = spaceTexture;
+    
+    // lumières
+    const pointLight = new THREE.PointLight(0xffffff);
+    pointLight.position.set(5,5,5);
+    const ambientLight = new THREE.AmbientLight(0xffffff);
+    scene.add(pointLight, ambientLight);
+    
+    // pour m'aider lors du développement à visualiser comment placer mes éléments
+    const lightHelper = new THREE.PointLightHelper(pointLight);
+    const gridHelper = new THREE.GridHelper(200,50);
+    scene.add(lightHelper, gridHelper);
+    
+    // contrôles
+    const controls = new OrbitControls(camera, renderer.domElement);
+    
+    // gère l'animation des objets
+    var animate = function () {
+      requestAnimationFrame( animate );
+      torus.rotation.x += 0.01;
+      torus.rotation.y += 0.005;
+      torus.rotation.z += 0.01;
+
+      controls.update();
+      renderer.render( scene, camera );
+    };
+    animate();
+  }
+
+  //   ce que l'on va afficher
+  render() {
+    return (
+        <main>
+            <h1> test</h1>
+        <div  ref={ref => (this.mount = ref)} className="fixed "> </div>
+      </main>
+    )
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
+/*
 const builder = imageUrlBuilder(sanityClient);
 function urlFor(source) {
     return builder.image(source);
 }
-
+*/
 export default function About() {
-    const [author, setAuthor] = useState(null);
+    /*const [author, setAuthor] = useState(null);
 
     useEffect(()=> {
         sanityClient.fetch(`*[_type == "author"] {
@@ -20,8 +106,8 @@ export default function About() {
         }`).then((data) => setAuthor(data[0])).catch(console.error);
         }, []);
         if(!author) return <div> En cours de chargement...</div>;
-
-        return (
+        */
+        return /*(
             <main className="relative"> 
                 <img src={backgroundComputerScience} alt="setup pour programmer" className="absolute w-full" />
                 <div className="p-10 lg:pt-48 container mx-auto relative">
@@ -36,6 +122,6 @@ export default function About() {
                         </div>
                     </section>
                 </div>
-            </main>
-        )
+            </main> 
+        )*/
 }
